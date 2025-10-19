df71e9829d07bda81efed2a3ef0e312f3a5338e5
Gideon Monterosa <gideon.monterosa@gmail.com>
2025-10-19 17:34:28 +0200

    add some keybinds for window management

--- home.nix
+++ home.nix
@@ -17,16 +17,24 @@
     enable = true;
     settings = {
       input.kb_layout = "ch";
-      monitor = "Virtual-1, 3072x1920, auto, 2.5";
+      monitor = "Virtual-1, 3072x1920, auto, 3";
 
       general = {
         "$mainMod" = "SUPER";
         "$terminal" = "foot";
+        "$browser" = "";
       };
 
       bind = [
         "$mainMod, Return, exec, $terminal"
         "$mainMod, M, exit,"
+        "$mainMod, Q, killactive,"
+
+        "$mainMod, h, movefocus, l"
+        "$mainMod, j, movefocus, d"
+        "$mainMod, k, movefocus, u"
+        "$mainMod, l, movefocus, r"
+
       ];
     };
   };
