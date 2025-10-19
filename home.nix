c95c15a865775df343d7e394cab535e84fe18ffb
Gideon Monterosa <gideon.monterosa@gmail.com>
2025-10-19 18:42:38 +0200

    add firefox and worspace keybinds

--- home.nix
+++ home.nix
@@ -5,6 +5,7 @@
   home.homeDirectory = "/home/gideon";
 
   home.packages = with pkgs; [
+    firefox
   ];
 
   programs.git = {
@@ -22,11 +23,13 @@
       general = {
         "$mainMod" = "SUPER";
         "$terminal" = "foot";
-        "$browser" = "";
+        "$browser" = "firefox";
       };
 
       bind = [
         "$mainMod, Return, exec, $terminal"
+        "$mainMod, b, exec, $browser"
+
         "$mainMod, M, exit,"
         "$mainMod, Q, killactive,"
 
@@ -35,6 +38,32 @@
         "$mainMod, k, movefocus, u"
         "$mainMod, l, movefocus, r"
 
+        "$mainMod SHIFT, h, movewindow, l"
+        "$mainMod SHIFT, j, movewindow, d"
+        "$mainMod SHIFT, k, movewindow, u"
+        "$mainMod SHIFT, l, movewindow, r"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
       ];
     };
   };
